a162628162325bee9d00d48542716a677b2ae841
Suger13 <muteki.scarlette@gmail.com>
2021-12-12 19:13:44 +0700

    added interval in prop aun.js

--- src/database/aunDB.js
+++ src/database/aunDB.js
@@ -1,9 +1,15 @@
 //สร้าง object ชื่อ aun แล้วเก็บ property เป็น ชื่อ อายุ และ ส่วนสูง ตามลำดับ
+let i = 0;
+
 
 const aun = {
     name : "aun",
     age : 31,
-    height : 170
+    height : 170,
+    loop : setInterval(() => {
+        console.log(i)
+        
+    }, 1000)
 }
 
 //เป็นการexport object aun ออกไปเพื่อให้สามารถเรียกใช้งานจากภายนอกได้ โดยการระบุตำแหน่งไฟล์นี้ในไฟล์อื่น ตัวอย่างการเรียกใช้ดูได้จาก ./controller/aun-controller.js
